20c22849f95755094775cd33e550960c5f7dd57a
Suaid Ishak <suaid.ishak@gmail.com>
2016-10-12 06:51:49 +0800

    5th

--- read_clean_starstx.R
+++ read_clean_starstx.R
@@ -21,8 +21,25 @@ stars_master_data$lon <- lon
 stars_7_days_dt <- read.csv("stock level.csv")
 cnames <- c("Day_1","Day_2","Day_3","Day_4","Day_5","Day_6","Day_7","Station_Name","Station ID","Station_Region","Diesel_Quota","Tank_ID","Product_Name","Day_1_Sales","Day_1_Stock","Day_2_Sales","Day_2_Stock","Day_3_Sales","Day_3_Stock","Day_4_Sales","Day_4_Stock","Day_5_Sales","Day_5_Stock","Day_6_Sales","Day_6_Stock","Day_7_Sales","Day_7_Stock")
 names(stars_7_days_dt) <- cnames
-stars_7_days_dt$Start_Date_data <- stars_7_days_dt$Day_1
-stars_7_days_dt$End_Date_data <- stars_7_days_dt$Day_7
+date_range <- stars_7_days_dt[1,1:7]
+cnames <- c(paste(date_range[1,1],"sales",sep = "_"),
+            paste(date_range[1,1],"stock",sep = "_"),
+            paste(date_range[1,2],"sales",sep = "_"),
+            paste(date_range[1,2],"stock",sep = "_"),
+            paste(date_range[1,3],"sales",sep = "_"),
+            paste(date_range[1,3],"stock",sep = "_"),
+            paste(date_range[1,4],"sales",sep = "_"),
+            paste(date_range[1,4],"stock",sep = "_"),
+            paste(date_range[1,5],"sales",sep = "_"),
+            paste(date_range[1,5],"stock",sep = "_"),
+            paste(date_range[1,6],"sales",sep = "_"),
+            paste(date_range[1,6],"stock",sep = "_"),
+            paste(date_range[1,7],"sales",sep = "_"),
+            paste(date_range[1,7],"stock",sep = "_"))
+cnames <- c("Day_1","Day_2","Day_3","Day_4","Day_5","Day_6","Day_7","Station_Name","Station ID","Station_Region","Diesel_Quota","Tank_ID","Product_Name",cnames)
+names(stars_7_days_dt) <- cnames
+#stars_7_days_dt$Start_Date_data <- stars_7_days_dt$Day_1
+#stars_7_days_dt$End_Date_data <- stars_7_days_dt$Day_7
 # txdata <- txdata[-c("Day_1","Day_2","Day_3","Day_4","Day_5","Day_6","Day_7")]
 
 stars_7_days_dt <- stars_7_days_dt[,-(1:7),drop=FALSE]
