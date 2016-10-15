2597d1314fc9066453fd8d04a8cd5fdd1a4695ee
Suaid Ishak <suaid.ishak@gmail.com>
2016-10-15 17:32:26 +0800

    tt

--- map example.R
+++ map example.R
@@ -1,44 +1,25 @@
-x <- c("ggmap", "rgdal", "rgeos", "maptools", "dplyr", "tidyr", "tmap")
+x <- c("ggmap", "rgdal", "rgeos", "maptools", "dplyr", "tidyr", "tmap","googleVis","ggplot2","data.table","raster","sp","shapefiles","rworldmap")
 lapply(x, library, character.only = TRUE) # load the required packages
+x <- CRS("+proj=longlat +ellps=WGS84")
+mys <- readShapePoly("data/MYS_adm1",verbose = T,proj4string = x)
+extent(mys)
+head(mys@data)
+wn <- getMap(resolution = "coarse")
+plot(wn)
+data("wrld_simpl",package = "maptools")
+plot(wrld_simpl,add = T)
 
-mysadmin <- readOGR(dsn = "data", layer = "MYS_adm2")
+plot(mys,add=T,axes=T,border="red")
+selangor <- subset(mys, mys$NAME_1=="Selangor")
+plot(selangor,add=T,col="blue")
+#mysadmin <- readOGR(dsn = "data", layer = "MYS_adm2")
 
-head(lnd@data, n = 2)
-mean(lnd$Partic_Per)
-head(lnd@polygons[[1]]@Polygons[[1]]@coords, 3)
-plot(lnd@polygons[[1]]@Polygons[[1]]@coords)
-# /Users/sratne/Documents/Tableau Datasets/Data Science /MYS_adm_shp/R_GIS_data/MYS_adm_shp
-# Select zones where sports participation is between 20 and 25%
-sel <- lnd$Partic_Per > 20 & lnd$Partic_Per < 25
-plot(lnd[sel, ]) # output not shown here
-head(sel) # test output of previous selection (not shown)
-plot(lnd, col = "lightgrey") # plot the london_sport object
-sel <- lnd$Partic_Per > 25
-plot(lnd[ sel, ], col = "turquoise", add = TRUE) # add selected zones to map
-EPSG <- make_EPSG() # create data frame of available EPSG codes
-EPSG[grepl("WGS 84$", EPSG$note), ] # search for WGS 84 code
-lnd84 <- spTransform(lnd, CRS("+init=epsg:4326")) # reproject
-# Save lnd84 object (we will use it in Part IV)
-saveRDS(object = lnd84, file = "data/lnd84.Rds")
-# Create and look at new crime_data object
-crime_data <- read.csv("data/mps-recordedcrime-borough.csv",stringsAsFactors = FALSE)
-head(crime_data, 3) # display first 3 lines
-head(crime_data$CrimeType) # information about crime type
-# Extract "Theft & Handling" crimes and save
-crime_theft <- crime_data[crime_data$CrimeType == "Theft & Handling", ]
-head(crime_theft, 2) # take a look at the result (replace 2 with 10 to see more rows)
-# Calculate the sum of the crime count for each district, save result
-crime_ag <- aggregate(CrimeCount ~ Borough, FUN = sum, data = crime_theft)
-# Show the first two rows of the aggregated crime data
-head(crime_ag, 2)
-# Compare the name column in lnd to Borough column in crime_ag to see which rows match.
-lnd$name %in% crime_ag$Borough
 
-mal_filepath = "/Users/sratne/Documents/Tableau Datasets/Data Science /MYS_adm_shp/R_GIS_data/MYS_adm_shp"
-head(mys@data, n = 2)
+#mal_filepath = "/Users/sratne/Documents/Tableau Datasets/Data Science /MYS_adm_shp/R_GIS_data/MYS_adm_shp"
+#$head(mys@data, n = 2)
 # mean(mys$Partic_Per)
-head(mys@polygons[[1]]@Polygons[[1]]@coords, 3)
-plot(mys@polygons[[1]]@Polygons[[1]]@coords)
+#head(mys@polygons[[1]]@Polygons[[1]]@coords, 3)
+#plot(mys@polygons[[1]]@Polygons[[1]]@coords)
 
 map.selangor <- subset(mysadmin, mysadmin$NAME_1 == "Selangor")
 data.sel <- stars_master_data[stars_master_data$State == "SEL",]
